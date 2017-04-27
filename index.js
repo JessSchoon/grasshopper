f2767d0169bd3a67c0f01e73c35bb1191b2c86b0
Jess Schoon <mail@jessicaschoonmaker.com>
2017-04-27 09:59:41 -0700

    added pug

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require("express")
 var application = express()
 
-application.get( "/", function (request, response) {
-  response.send("Hi!")
+application.get( "/:recipe", function (request, response) {
+  response.render("index", {recipe: request.param("recipe")})
 })
 
 application.listen(1436)
+
+application.set('view engine', 'pug')
+application.set('views', './views')
