44d0c689cceaef1d1257996b936cdbdd2ea2fe69
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2017-05-10 21:38:10 -0700

    Integrate nodemon and standard.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var express = require("express")
-var path = require("path")
+var express = require('express')
+var path = require('path')
 var application = express()
 
-application.get("/", function (request, response){
-  response.render("index")
+application.get('/', function (request, response) {
+  response.render('index')
 })
 
-application.get( "/bootstrap.js", function (request, response) {
-  var jsPath = path.join(__dirname, "node_modules", "bootstrap", "dist", "js", "bootstrap.js")
+application.get('/bootstrap.js', function (request, response) {
+  var jsPath = path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js', 'bootstrap.js')
   response.sendFile(jsPath)
 })
 
-application.get( "/bootstrap.css", function (request, response) {
-  var cssPath = path.join(__dirname, "node_modules", "bootstrap", "dist", "css", "bootstrap.css")
+application.get('/bootstrap.css', function (request, response) {
+  var cssPath = path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css')
   response.sendFile(cssPath)
 })
 
-application.get("/styles.css", function(request, response) {
-  var cssPath = path.join(__dirname, "assets", "styles.css")
+application.get('/styles.css', function (request, response) {
+  var cssPath = path.join(__dirname, 'assets', 'styles.css')
   response.sendFile(cssPath)
 })
 
-application.get( "/:recipe", function (request, response) {
-  var recipeData = require("./recipes.json")
+application.get('/:recipe', function (request, response) {
+  var recipeData = require('./recipes.json')
   var data = recipeData.data[0]
-  response.render("recipe", data)
+  response.render('recipe', data)
 })
 
 application.listen(1436)
