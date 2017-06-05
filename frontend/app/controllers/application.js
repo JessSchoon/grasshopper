8e1f9eea9a64ba58207b034e1a9833082f232605
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2017-06-05 10:02:40 -0700

    Move recipe index to its own route.

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  recipes: [],
-  init: function () {
-    this.get('store')
-      .findAll('recipe')
-      .then(recipes => this.set('recipes', recipes))
-  }
 });
